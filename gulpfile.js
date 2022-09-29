46aa54e4c1c176d84f756203a3777d23ed5b0061
James Alderson <james.alderson.a@gmail.com>
2022-09-29 19:29:16 +0330

    Create 'Solutions' section
    - Add 'bootstrap'.
    - Create 'main.css'.
    - Update 'gulpfile.js'.
    - Don't compress favicon files.
    - Uninstall unnecessary packages.

--- gulpfile.js
+++ gulpfile.js
@@ -1,79 +1,51 @@
-// Initialize Modules
+// Initialize modules
 const { src, dest, watch, series, parallel } = require("gulp")
 
 // Import packages
-const sass = require("gulp-sass")(require("sass"))
 const postcss = require("gulp-postcss")
 const autoprefixer = require("autoprefixer")
 const cssnano = require("cssnano")
-const concat = require("gulp-concat")
-const terser = require("gulp-terser")
-const imagemin = require("gulp-imagemin")
-const avif = require("gulp-avif")
 const replace = require("gulp-replace")
-const ttf2woff = require("gulp-ttf2woff")
 const browser_sync = require("browser-sync").create()
 
-// Files Path
+// Files path
 const files = {
-  scssPath: "src/scss/**/*.scss",
-  jsPath: "src/js/**/*.js",
-  imagePath: "src/images/**/*",
-  screenPath: "src/screenshots/**/*.png",
-  fontPath: "src/fonts/**/*.ttf"
+  cssPath: "src/css/*.css",
 }
 
-// Scss Task
-function scss_Task() {
-  return src(files.scssPath, { sourcemaps: true })
-    .pipe(sass().on("error", sass.logError))
+/* Css task
+    1. Add vendor prefixes
+    2. Minify css
+============ */
+function css_task() {
+  return src(files.cssPath)
     .pipe(postcss([autoprefixer("last 4 versions"), cssnano()]))
-    .pipe(dest("dist/css", { sourcemaps: "." }))
+    .pipe(dest("dist/css"))
 }
 
-// JavaScript Task
-function js_Task() {
-  return src(files.jsPath)
-    .pipe(concat("script.js"))
-    .pipe(terser())
-    .pipe(dest("dist/js"))
-}
-
-// Imagemin Task
-function imagemin_Task() {
-  return src(files.imagePath)
-    .pipe(imagemin([
-      imagemin.mozjpeg({ quality: 75, progressive: true }),
-      imagemin.optipng({ optimizationLevel: 5 }),
-    ]).on("error", error => console.log(error)))
-    .pipe(dest("dist/images"))
-}
-
-// Convert Images to Avif format
-function avif_Task() {
-  return src(files.screenPath)
-    .pipe(avif())
-    .pipe(dest("dist/screenshots"))
-}
-
-// Font Task
-function font_task() {
-  return src(files.fontPath)
-    .pipe(ttf2woff())
-    .pipe(dest("dist/fonts"));
-}
-
-// Cachebust Task
-function cachebust_Task() {
-  let dateString = new Date().getTime()
+// Cachebust task
+function cachebust_task() {
+  let timeString = new Date().getTime()
 
   return src(["index.html"])
-    .pipe(replace(/cb=\d+/g, "cb=" + dateString))
+    .pipe(replace(/cb=\d+/g, "cb=" + timeString))
     .pipe(dest("."))
 }
 
-// BrowserSync Task
-function browserSync_Task(callback) {
+// CopyCss vendors task
+function copyCss_task() {
+  return src(["src/css/vendors/**/*"], { "base": "src/css" })
+    .pipe(dest("dist/css"))
+}
+
+// CopyImage task
+function copyImage_task() {
+  return src(["src/images/favicon/*"], { "base": "src/images" })
+    .pipe(dest("dist/images"))
+}
+
+// Initialize browserSync
+function browserSync(callback) {
   browser_sync.init({
     server: {
       baseDir: "."
@@ -87,22 +59,26 @@ function browserSync_Task(callback) {
   })
   callback()
 }
+
+// Reload browserSync
 function browserSyncReload(callback) {
   browser_sync.reload()
   callback()
 }
 
-// Watch Task
+// BrowserSync watch task
+function browserSyncWathc() {
+  watch(["index.html"], browserSyncReload)
+  watch([files.cssPath], series(parallel(css_task), cachebust_task))
+}
+
+// Gulp watch task
 function watch_Tasks() {
-  watch([files.scssPath, files.jsPath], series(parallel(scss_Task, js_Task), cachebust_Task))
+  watch([files.cssPath], series(parallel(css_task), cachebust_task))
 }
 
-// BrowserSync Watch Task
-function bsWatch_Task() {
-  watch("index.html", browserSyncReload)
-  watch([files.scssPath, files.jsPath], series(parallel(scss_Task, js_Task), cachebust_Task))
-}
+// Gulp default task
+exports.default = series(parallel(css_task), cachebust_task, copyCss_task, copyImage_task, watch_Tasks)
 
-// Gulp Default Task
-exports.default = series(parallel(scss_Task, js_Task, font_task), cachebust_Task, imagemin_Task, avif_Task, watch_Tasks)
-exports.bs = series(parallel(scss_Task, js_Task, font_task), cachebust_Task, imagemin_Task, avif_Task, browserSync_Task, bsWatch_Task)
+// Run browserSync
+exports.bs = series(parallel(css_task), cachebust_task, copyCss_task, copyImage_task, browserSync, browserSyncWathc)
